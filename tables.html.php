71ffe3b748f699aa4ab271aca40ec9915c7cd312
Ryan King <rking2658@gmail.com>
2017-04-24 20:50:18 -0600

    updated tables.html.php

--- tables.html.php
+++ tables.html.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap Admin Theme v3</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Job List</title>
     <!-- jQuery UI -->
     <link href="https://code.jquery.com/ui/1.10.3/themes/redmond/jquery-ui.css" rel="stylesheet" media="screen">
 
@@ -25,12 +28,12 @@
 	           <div class="col-md-5">
 	              <!-- Logo -->
 	              <div class="logo">
-	                 <h1><a href="index.html">Work Order</a></h1>
+	                 <h1><a href="index.html">Job List</a></h1>
 	              </div>
 	           </div>
 	           <div class="col-md-5">
 	              <div class="row">
-	                <div class="col-lg-12">
+	                <div class="col-md-12">
 	                  <div class="input-group form">
 	                       <input type="text" class="form-control" placeholder="Search...">
 	                       <span class="input-group-btn">
@@ -87,58 +90,57 @@
 	            </div>
 			</div>
 
-			<!-- Location filter select -->
-			<div class="col-md-5">
-	          <div class="row">
-	            <div class="col-lg-12">
-	              <div class="input-group form">
-	              	<form method="post" action="select1.php">
-	              	  <select name="sort_id">  
-				        <option value="job_location">School</option>
-				      </select>
-				      <select name="sort_selection">
-				        <option value=""></option>
-	                      <option value="Anna Smith Elementary">Anna Smith Elementary</option>
-	                      <option value="Blue Peak High">Blue Peak High</option>
-	                      <option value="Clarke N Johnsen Junior High">Clarke N Johnsen Junior High</option>
-	                      <option value="Community Learning Center">Community Learning Center</option>
-	                      <option value="Copper Canyon Elementary">Copper Canyon Elementary</option>
-	                      <option value="Dugway">Dugway</option>
-	                      <option value="Early Learning Center">Early Learning Center</option>
-	                      <option value="Grantsville Elementary">Grantsville Elementary</option>
-	                      <option value="Grantsville High">Grantsville High</option>
-	                      <option value="Grantsville Junior High">Grantsville Junior High</option>
-	                      <option value="Ibapah Elementary">Ibapah Elementary</option>
-	                      <option value="Middle Canyon Elementary">Middle Canyon Elementary</option>
-	                      <option value="North Lake Elementary">North Lake Elementary</option>
-	                      <option value="Old Mill Elementary">Old Mill Elementary</option>
-	                      <option value="Overlake Elementary">Overlake Elementary</option>
-	                      <option value="Rose Springs Elementary">Rose Springs Elementary</option>
-	                      <option value="Settlement Canyon Elementary">Settlement Canyon Elementary</option>
-	                      <option value="Stansbury High">Stansbury High</option>
-	                      <option value="Stansbury Park Elementary">Stansbury Park Elementary</option>
-	                      <option value="Sterling Elementary">Sterling Elementary</option>
-	                      <option value="Tooele High">Tooele High</option>
-	                      <option value="Tooele Junior High">Tooele Junior High</option>
-	                      <option value="Vernon Elementary">Vernon Elementary</option>
-	                      <option value="Wendover High">Wendover High</option>
-	                      <option value="West Elementary">West Elementary</option>
-	                      <option value="Willow Elementary">Willow Elementary</option>
-				      </select>
-				      <button type="submit" value="execute">Filter</button>
-				    </form>
-	              </div>
-	            </div>
-	          </div>
-	       	</div>
-
 			<!-- Bootstrap table -->
 			
 			<div class="col-md-10">
 			  	<div class="row">
 					<div class="content-box-large">
 			  			<div class="panel-heading">
-							<div class="panel-title">Bootstrap dataTables</div>
+							<!-- Location filter select -->
+							<div class="col-md-5">
+					          <div class="row">
+					            <div class="col-md-12">
+					              <div class="input-group form">
+					              	<form method="post" action="select1.php">
+					              	  <select name="sort_id">  
+								        <option value="job_location">School</option>
+								      </select>
+								      <select name="sort_selection">
+								        <option value=""></option>
+					                      <option value="Anna Smith Elementary">Anna Smith Elementary</option>
+					                      <option value="Blue Peak High">Blue Peak High</option>
+					                      <option value="Clarke N Johnsen Junior High">Clarke N Johnsen Junior High</option>
+					                      <option value="Community Learning Center">Community Learning Center</option>
+					                      <option value="Copper Canyon Elementary">Copper Canyon Elementary</option>
+					                      <option value="Dugway">Dugway</option>
+					                      <option value="Early Learning Center">Early Learning Center</option>
+					                      <option value="Grantsville Elementary">Grantsville Elementary</option>
+					                      <option value="Grantsville High">Grantsville High</option>
+					                      <option value="Grantsville Junior High">Grantsville Junior High</option>
+					                      <option value="Ibapah Elementary">Ibapah Elementary</option>
+					                      <option value="Middle Canyon Elementary">Middle Canyon Elementary</option>
+					                      <option value="North Lake Elementary">North Lake Elementary</option>
+					                      <option value="Old Mill Elementary">Old Mill Elementary</option>
+					                      <option value="Overlake Elementary">Overlake Elementary</option>
+					                      <option value="Rose Springs Elementary">Rose Springs Elementary</option>
+					                      <option value="Settlement Canyon Elementary">Settlement Canyon Elementary</option>
+					                      <option value="Stansbury High">Stansbury High</option>
+					                      <option value="Stansbury Park Elementary">Stansbury Park Elementary</option>
+					                      <option value="Sterling Elementary">Sterling Elementary</option>
+					                      <option value="Tooele High">Tooele High</option>
+					                      <option value="Tooele Junior High">Tooele Junior High</option>
+					                      <option value="Vernon Elementary">Vernon Elementary</option>
+					                      <option value="Wendover High">Wendover High</option>
+					                      <option value="West Elementary">West Elementary</option>
+					                      <option value="Willow Elementary">Willow Elementary</option>
+								      </select>
+								      <button type="submit" value="execute">Filter</button>
+								    </form>
+					              </div>
+					            </div>
+					          </div>
+					       	</div>
+
 		  					<div class="panel-body">
 		  						<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="example">
 									<thead>
